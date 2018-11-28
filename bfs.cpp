8fa9bc40bcef2b5f544799fcfc3daba51c7e0b6d
User <mathewjkavalam@gmail.com>
2018-11-29 02:32:14 +0530

    completed bfs

--- bfs.cpp
+++ bfs.cpp
@@ -29,12 +29,51 @@ void Graph::add_edge ( int u , int v ) {
 
 void Graph::bfs ( int s ) {
     
+    bool *visited = new bool [ V ];
+    list<int> queue;
+    
+    for( int i = 0 ; i < V ; i++ ){
+    
+        visited [ i ] = false;
+    }
+    
+    queue.push_back ( s );
+    visited [ s ] = true;
+    
+    list<int>::iterator i;
+    
+    while ( !queue.empty() ) {    
+        
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+        
+        for ( i = adj[s].begin() ; i != adj[s].end() ; ++i ){
+                
+            if ( !visited[ *i ] ) {
+            
+                queue.push_back ( *i );
+                visited[ *i ] = true;
+            }                  
+        } 
+            
+    }    
     
 }
 
 int main () {
 
-    Graph test = Graph ( 10 );
+    Graph test = Graph ( 7 );
+    test.add_edge( 1, 2 );
+    test.add_edge( 1, 3 );
+    test.add_edge( 2, 4 );
+    test.add_edge( 2, 5 );
+    test.add_edge( 3, 6 );
+    test.add_edge( 6, 5 );
+    
+    test.bfs( 1 );
+    
+    return 0;
 }
         
     
